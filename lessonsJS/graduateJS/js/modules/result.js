163e929675a7c775389fe336fe7a669fc0384b56
Rainmaker <rainmakeroffire@gmail.com>
2023-01-25 16:29:22 +0300

    Defined 'show' and 'hide' methods of the Result class.

---
+++ lessonsJS/graduateJS/js/modules/result.js
@@ -0,0 +1,24 @@
+export default class Result {
+    constructor(element) {
+        this.counter = element;
+
+        this.root = element.querySelector(`.counter__result`);
+        this.caloriesNormOutput = this.root.querySelector(`#calories-norm`);
+        this.caloriesMinOutput = this.root.querySelector(`#calories-minimal`);
+        this.caloriesMaxOutput = this.root.querySelector(`#calories-maximal`);
+    }
+
+    show(calories) {
+        this.root.classList.remove("counter__result--hidden");
+        this.caloriesNormOutput.textContent = Math.floor(calories['NORM'] + 0.5);
+        this.caloriesMinOutput.textContent = Math.floor(calories['MIN'] + 0.5);
+        this.caloriesMaxOutput.textContent = Math.floor(calories['MAX'] + 0.5);
+    }
+
+    hide() {
+        this.root.classList.add("counter__result--hidden");
+        this.caloriesNormOutput.textContent = "";
+        this.caloriesMinOutput.textContent = "";
+        this.caloriesMaxOutput.textContent = "";
+    }
+}
